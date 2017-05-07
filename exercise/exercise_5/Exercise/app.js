992d4dadf278001c4a1f1c7012ffb8df16fc23ff
Tanakit <tanakit@hollywoodmovies.co.th>
2017-05-07 16:12:47 +0700

    update

---
+++ exercise/exercise_5/Exercise/app.js
@@ -0,0 +1,21 @@
+
+console.log(1111);
+
+new Vue({
+  el: '#exercise',
+  data: {
+    evenShow: true,
+    arrays: ['Max', 'Anna', 'Chris', 'Manu'],
+    myObject: {
+      title: 'Lord of the Rings',
+      author: 'J.R.R. Tolkiens',
+      books: '3'
+    },
+    testData: {
+      name: 'TESTOBJECT', 
+      id: 10,
+      data: [1.67, 1.33, 0.98, 2.21]
+    }
+  }
+});
+
